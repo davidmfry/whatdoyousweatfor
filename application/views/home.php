85dd918331668d2bb9e3016c8b534dc4e0ac20b6
calinblitiu <calin.blitiu@outlook.com>
2018-06-04 00:08:36 -0300

    assets

---
+++ application/views/home.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>What do you sweat for</title>
+
+		<!-- Bootstrap Core CSS -->
+		<link href="<?=base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
+
+		<link href="<?=base_url()?>assets/css/scrolling-nav.css" rel="stylesheet">
+		<link href="<?=base_url()?>assets/css/owl.carousel.min.css" rel="stylesheet">
+		
+		<link href="<?=base_url()?>assets/css/font-awesome.min.css" rel="stylesheet">
+		
+		<!-- Custom CSS -->
+		<link href="<?=base_url()?>assets/css/style.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+	</head>
+
+	<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+	
+	<!--<header class="header-area">
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+				<div class="container">
+					<div class="navbar-header page-scroll">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand page-scroll" href="#page-top"><img src="images/logo.png" alt="" /></a>
+					</div>
+
+					<div class="collapse navbar-collapse navbar-ex1-collapse">
+						<ul class="nav navbar-nav navbar-right main-menu">
+							<p>CALL US TODAY: <a href="tel:206-519-5450">206-519-5450</a></p>
+							<li class="hidden">
+								<a class="page-scroll" href="#page-top"></a>
+							</li>
+							<li>
+								<a class="page-scroll" href="#page-top">Home</a>
+							</li>
+							<li>
+								<a class="page-scroll" href="#galarry">Galarry</a>
+							</li>
+							<li>
+								<a class="page-scroll" href="#contact">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</header>/Header Area-->
+		
+		
+	<div class="page-wrapper">
+	
+		<div class="top-area">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="top-logo">
+							<a href=""><img src="<?=base_url()?>assets/images/logo.png" alt="" /></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+		<!-- Slider Section
+		<section class="slider-area">
+			<div class="owl-carousel sliders">
+				<div class="single-slider" style="background:url(images/slider/1.jpg) no-repeat">
+					<div class="display-table">
+						<div class="table-cell">
+							<div class="slider-content">
+								<h1>Free pass</h1>
+								<p>Join today and claim your free one-day class pass.</p>
+								<a href="">Free pass</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="single-slider" style="background:url(images/slider/2.jpg) no-repeat">
+					<div class="display-table">
+						<div class="table-cell">
+							<div class="slider-content">
+								<h1>What's your style?</h1>
+								<p>Join today and claim your free one-day class pass.</p>
+								<a href="">Styles</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="single-slider" style="background:url(images/slider/3.jpg) no-repeat">
+					<div class="display-table">
+						<div class="table-cell">
+							<div class="slider-content">
+								<h1>Suited just for you</h1>
+								<p>Join today and claim your free one-day class pass.</p>
+								<a href="">Membership option</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="single-slider" style="background:url(images/slider/4.jpg) no-repeat">
+					<div class="display-table">
+						<div class="table-cell">
+							<div class="slider-content">
+								<h1>Live healthy program</h1>
+								<p>Join today and claim your free one-day class pass.</p>
+								<a href="">Stay strong</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="single-slider" style="background:url(images/slider/5.jpg) no-repeat">
+					<div class="display-table">
+						<div class="table-cell">
+							<div class="slider-content">
+								<h1>Free pass</h1>
+								<p>Join today and claim your free one-day class pass.</p>
+								<a href="">Free pass</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>-->
+	
+		<!--hero area-->
+		<section class="text-area section-padding dark-overlay" style="background: url(<?=base_url()?>assets/images/3.jpg) no-repeat">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="text-content">
+							<h2>What do you sweat for. Burn calories. Burn inches. Burn with a fire inside. Tell the world what you burn for.</h2>
+						</div>
+						<div id="image-preview">
+						  <label for="image-upload" id="image-label">Choose File</label>
+						  <input type="file" name="image" id="image-upload" />
+						</div>
+						<div class="text-content">
+							<h2>Not a member? <br />Try Orangetheory Fitness for free. <br /><a href="">Start burning today</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<section class="galarry-area section-padding">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-4">
+						<img src="<?=base_url()?>assets/images/sample/sample-1.jpg" alt="" />
+					</div>
+					<div class="col-md-4">
+						<img src="<?=base_url()?>assets/images/sample/sample-2.jpg" alt="" />
+					</div>
+					<div class="col-md-4">
+						<img src="<?=base_url()?>assets/images/sample/sample-3.jpg" alt="" />
+					</div>
+					<div class="col-md-4">
+						<img src="<?=base_url()?>assets/images/sample/sample-4.jpg" alt="" />
+					</div>
+					<div class="col-md-4">
+						<img src="<?=base_url()?>assets/images/sample/sample-5.jpg" alt="" />
+					</div>
+					<div class="col-md-4">
+						<img src="<?=base_url()?>assets/images/sample/sample-6.jpg" alt="" />
+					</div>
+					<div class="col-md-4">
+						<img src="<?=base_url()?>assets/images/sample/sample-7.jpg" alt="" />
+					</div>
+					<div class="col-md-4">
+						<img src="<?=base_url()?>assets/images/sample/sample-8.jpg" alt="" />
+					</div>
+					<div class="col-md-4">
+						<img src="<?=base_url()?>assets/images/sample/sample-9.jpg" alt="" />
+					</div>
+					<div class="col-md-4">
+						<img src="<?=base_url()?>assets/images/sample/sample-10.jpg" alt="" />
+					</div>
+					<div class="col-md-4">
+						<img src="<?=base_url()?>assets/images/sample/sample-11.jpg" alt="" />
+					</div>
+					<div class="col-md-4">
+						<img src="<?=base_url()?>assets/images/sample/sample-12.jpg" alt="" />
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		
+		<footer class="footer-area">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+						<div class="footer-link">
+							<a href="">About</a>
+							<a href="">Privacy</a>
+							<a href="">Terms</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>	
+	
+	
+		<!-- jQuery -->
+		<script src="<?=base_url()?>assets/js/jquery.js"></script>
+
+		<!-- Bootstrap Core JavaScript -->
+		<script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
+
+		<!-- Scrolling Nav JavaScript -->
+		<script src="<?=base_url()?>assets/js/jquery.easing.min.js"></script>
+		<script src="<?=base_url()?>assets/js/scrolling-nav.js"></script>
+		
+		<script src="<?=base_url()?>assets/js/owl.carousel.min.js"></script>
+		<script src="<?=base_url()?>assets/js/jquery.uploadPreview.min.js"></script>
+		<script src="<?=base_url()?>assets/js/main.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+			  $.uploadPreview({
+				input_field: "#image-upload",   // Default: .image-upload
+				preview_box: "#image-preview",  // Default: .image-preview
+				label_field: "#image-label",    // Default: .image-label
+				label_default: "Choose File",   // Default: Choose File
+				label_selected: "Change File",  // Default: Change File
+				no_label: false                 // Default: false
+			  });
+			});
+		</script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+			  $.uploadPreview({
+				input_field: "#image-upload",
+				preview_box: "#image-preview",
+				label_field: "#image-label"
+			  });
+			});
+		</script>
+
+	</body>
+
+</html>
